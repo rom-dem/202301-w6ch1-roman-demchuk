b7bf2d3b480627a9e6a69c55d0e9fcd6249cbd79
Róman Demchuk <roman.demchuk@gmail.com>
2023-02-14 20:06:48 +0100

    Add Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import "./App.css";
+import Form from "./components/Form/Form";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>My To Do list</h1>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
